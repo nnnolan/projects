775249b572f682e8d54321bc776d819942f9b165
Nolan <pestanonolan@gmail.com>
2022-02-14 08:07:55 -0600

    wip

---
+++ broadcast/main.js
@@ -0,0 +1,9 @@
+emoji = document.getElementById("main");
+button = document.getElementsByTagName("button");
+
+
+
+
+button.addEventListener("click", function(){
+    button.textContent = "Clicked!";
+});
